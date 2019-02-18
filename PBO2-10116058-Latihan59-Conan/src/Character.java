8f7b8fadea24e72f5a6b9be303a69dabfcb18303
nialditanawara <31877365+nialditanawara@users.noreply.github.com>
2019-02-19 02:09:29 +0700

    Add files via upload

---
+++ PBO2-10116058-Latihan59-Conan/src/Character.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116058.latihan59.conan;
+
+public class Character {
+
+    protected String name;
+    protected String from;
+    protected boolean eyeglasses;
+
+    public Character(String name, String from, boolean eyeglasses) {
+        this.name = name;
+        this.from = from;
+        this.eyeglasses = eyeglasses;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+}
